d7917e8e4665f20e99c993d1857b57930fddb02c
RUDRAKSHA KUSHWAHA <kushwaharudraksha@gmail.com>
2024-09-16 23:25:50 +0530

    Add files via upload

---
+++ src/conn.java
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+import java.sql.*;  
+
+public class conn{
+    Connection c;
+    Statement s;
+    public conn(){  
+        try{  
+            Class.forName("com.mysql.jdbc.Driver");  
+            c =DriverManager.getConnection("jdbc:mysql:///hotelmanagementsystem","root","CSworm123");
+            
+            s =c.createStatement();  
+            
+           
+        }catch(Exception e){ 
+            System.out.println(e);
+        }  
+    }  
+}  
+
